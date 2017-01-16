9bce715f203f30adb48e784e3a2c8890d64d79d3
Sven Strittmatter <sven.strittmatter@iteratec.de>
2017-01-16 09:53:49 +0100

    Implement REPL.

--- main.go
+++ main.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"fmt"
-	//	"monkey/repl"
-	//	"os"
+	"github.com/Weltraumschaf/monkey/repl"
+	"os"
 )
 
 func main() {
 	fmt.Printf("Feel free to type in commands\n")
-	//	repl.Start(os.Stdin, os.Stdout)
+	repl.Start(os.Stdin, os.Stdout)
 }
