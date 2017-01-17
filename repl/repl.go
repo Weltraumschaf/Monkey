db945c2a940eaf6ab25ed4c9324889cd742087a3
Sven Strittmatter <sven.strittmatter@iteratec.de>
2017-01-17 22:26:56 +0100

    Add variable binding.

--- repl/repl.go
+++ repl/repl.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/Weltraumschaf/monkey/evaluator"
 	"github.com/Weltraumschaf/monkey/lexer"
+	"github.com/Weltraumschaf/monkey/object"
 	"github.com/Weltraumschaf/monkey/parser"
 	"io"
 )
@@ -13,6 +14,7 @@ const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+	env := object.NewEnvironment()
 
 	for {
 		fmt.Printf(PROMPT)
@@ -32,7 +34,7 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		evaluated := evaluator.Eval(program)
+		evaluated := evaluator.Eval(program, env)
 
 		if evaluated != nil {
 			io.WriteString(out, evaluated.Inspect())
