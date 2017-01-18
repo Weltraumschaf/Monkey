942205d04bad1887e37bb32f2907cfc61005d483
Sven Strittmatter <sven.strittmatter@iteratec.de>
2017-01-18 16:23:17 +0100

    Implement built in function to put something to STDOUT.

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -1,6 +1,9 @@
 package evaluator
 
-import "github.com/Weltraumschaf/monkey/object"
+import (
+	"fmt"
+	"github.com/Weltraumschaf/monkey/object"
+)
 
 var builtins = map[string]*object.Builtin{
 	"len": &object.Builtin{
@@ -99,6 +102,15 @@ var builtins = map[string]*object.Builtin{
 			newElements[length] = args[1]
 
 			return &object.Array{Elements: newElements}
+		},
+	},
+	"puts": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+			for _, arg := range args {
+				fmt.Println(arg.Inspect())
+			}
+
+			return NULL
 		},
 	},
 }
