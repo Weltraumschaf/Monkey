ef4210d35e321da2c5d62b29d17e3308fa67ec5a
Sven Strittmatter <sven.strittmatter@iteratec.de>
2017-01-20 20:10:16 +0100

    Ignore generated code.

--- antlr/generate.sh
+++ antlr/generate.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env sh
 
-java -jar ${GOPATH}/src/github.com/antlr/antlr4/tool/target/antlr4-4.6-complete.jar -Dlanguage=Go -visitor -package antlr Monkey.g4
+antlr="${GOPATH}/src/github.com/antlr/antlr4/tool/target/antlr4-4.6-complete.jar"
+
+java -jar "${antlr}" -Dlanguage=Go -visitor -package antlr Monkey.g4
