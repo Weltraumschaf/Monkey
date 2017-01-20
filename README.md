224ea4cafef60242a8a44faa536fec3b5bc8ace0
Sven Strittmatter <sven.strittmatter@iteratec.de>
2017-01-20 11:26:19 +0100

    Add kingpin CLI.

--- README.md
+++ README.md
@@ -3,5 +3,7 @@
 ## What Todo Next
 
 1. Add CLI interface (`monkey repl|run`)
+    - [kingpin how-to](https://developer.atlassian.com/blog/2016/03/building-helpful-golang-cli-tools/)
+    - [kingpin]()https://github.com/alecthomas/kingpin)
 2. Read from file `monkey run <file>`
 3. Use [ANTLR](https://github.com/antlr/antlr4/blob/master/doc/go-target.md) as frontend
--- main.go
+++ main.go
@@ -2,11 +2,32 @@ package main
 
 import (
 	"fmt"
-	"github.com/Weltraumschaf/monkey/repl"
+	"gopkg.in/alecthomas/kingpin.v2"
 	"os"
+	"github.com/Weltraumschaf/monkey/repl"
+)
+
+var (
+	// See https://github.com/alecthomas/kingpin
+	app     = kingpin.New("monkey", "The Monkey programming language.")
+	debug   = app.Flag("debug", "Enable debug mode.").Short('d').Bool()
+	verbose = app.Flag("verbose", "Enable verbose mode.").Short('v').Bool()
+
+	replCmd = app.Command("repl", "Starts the REPL.")
+
+	runCmd = app.Command("run", "Interpret the given file.")
 )
 
 func main() {
-	fmt.Printf("Feel free to type in commands\n")
-	repl.Start(os.Stdin, os.Stdout)
+	kingpin.Version("1.0.0-SNAPSHOT")
+
+	switch kingpin.MustParse(app.Parse(os.Args[1:])) {
+	case replCmd.FullCommand():
+		fmt.Printf("Feel free to type in commands\n")
+		repl.Start(os.Stdin, os.Stdout)
+	case runCmd.FullCommand():
+		fmt.Println("Not implemented yet!")
+	default:
+		fmt.Println("Bad sub command!")
+	}
 }
